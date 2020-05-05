cc6f450a5d892200bb4f38c60225f2b013112f23
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-05-06 03:26:30 +0530

    Firebase Registration Done

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,7 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
+import {Link, withRouter} from 'react-router-dom';
 
+import { withFirebase } from '../firebase';
 
 const Register = () => {
     return(
@@ -10,28 +12,97 @@ const Register = () => {
                         <h1>Register</h1>
                     </div>
                     <div className="col-4">
-                        <form> 
-                            <legend>Register</legend>
-                            <div className="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                            </div>
-                            <div className="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1" />
-                            </div>
-                            <div className="form-group form-check">
-                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                                <label className="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                        </form>
+                        <Form />
                     </div>
                 </div>
             </div>
         </Fragment>
     );
 }
+
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    pass1: '',
+    pass2: '',
+    error: null
+};
+
+class RForm extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { ...INITIAL_STATE};
+
+        
+    }
+// eslint-disable-next-line
+    onSubmit = event => {
+        const {username, email, pass1} = this.state;
+        
+        this.props.firebase
+            .doCreateUserWithEmailAndPassword(email, pass1)
+            .then(authUser => {
+                this.setState({ ...INITIAL_STATE });
+
+                this.props.history.push('/login');
+            })
+            .catch(error => {
+                this.setState({ error });
+            });
+
+        event.preventDefault();
+    };
+
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    render(){
+        const {
+            username,
+            email,
+            pass1,
+            pass2,
+            error
+        } = this.state;
+
+        const isInvalid =
+            pass1 !== pass2 ||
+            pass1 === '' ||
+            email === '' ||
+            username === '';
+
+        return(
+            <form onSubmit={this.onSubmit}>
+                <legend>Register</legend>
+                <div className="form-group">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" className="form-control" id="username" name="username" value={username} onChange={this.onChange} placeholder="Username" />
+                    <small id="userHelp" className="form-text text-muted">Username should be lowercase.</small>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email address</label>
+                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" name="email" value={email} onChange={this.onChange} placeholder="Email" />
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="pass1">Password</label>
+                    <input type="password" className="form-control" id="pass1" name="pass1" value={pass1} onChange={this.onChange} placeholder="Enter Password" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="pass2">Confirm Password</label>
+                    <input type="password" className="form-control" id="pass2" name="pass2" value={pass2} onChange={this.onChange} placeholder="Confirm Password" />
+                </div>
+                <button type="submit" disabled={isInvalid} className="btn btn-primary mt-2">Register</button>
+                <p className="text-muted"><small>Already have an account? <Link to="/login">Login</Link></small></p>
+
+                {error && <p>{error.message}</p>}
+            </form>
+        );
+    }
+}
+
+const Form = withRouter(withFirebase(RForm));
 
 export default Register;
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,6 @@
-import FirebaseContext from './Context';
+import FirebaseContext, { withFirebase } from './Context';
 import Firebase from './Firebase';
  
 export default Firebase;
  
-export { FirebaseContext };
+export { FirebaseContext, withFirebase };
